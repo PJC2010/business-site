224a6047a4712164593431dae0fdfd3b0d9d993b
PJC2010 <castillop92@gmail.com>
2024-11-18 21:59:33 -0600

    added client

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from 'react';
 import { Mail, MapPin, Send, Linkedin, Github } from 'lucide-react';
 
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from 'react';
 import { ArrowRight, Menu, X } from 'lucide-react';
 
