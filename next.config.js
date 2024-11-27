1c7973ceb861f75d40f19a9af1c307e8474d8780
PJC2010 <castillop92@gmail.com>
2024-11-26 21:11:14 -0600

    fixing when broken

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      loader: 'custom',
-      domains: ['websitebuckt.s3.amazonaws.com'],
-      loaderFile: './imageLoader.js'
-    },
-    // Add any other Next.js config options you need
+const nextConfig = {// Add any other Next.js config options you need
   }
   
   module.exports = nextConfig
